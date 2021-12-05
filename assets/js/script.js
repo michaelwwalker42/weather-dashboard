5a00ca933899ee3d089a2b53d5720d18ec6892e8
Michael Walker <michaelwwalker42@gmail.com>
2021-12-05 00:18:47 -0700

    added function to fetch UV index

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,10 @@
 var apiKey = "9195677d0010c5ed8b3059b59c364e87"
-var oneCall = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude=minutely,hourly,alerts&units=imperial&appid={API key}"
-
-var getCoords = "api.openweathermap.org/data/2.5/weather?q={city name}&appid=" + apiKey
-// template literal
-// var temp = `https://api.openweathermap.org/data/2.5/weather?q={city name}&appid=${apiKey}`
-
 
 var searchInput = document.getElementById("searchInput");
 var searchForm = document.getElementById("searchForm");
 var current = document.getElementById('current');
-var now = (dayjs().format('(M/DD/YYYY)'));
-
-
+var now = (dayjs().format(' (M/DD/YYYY)'));
+var currentCity = document.getElementById("currentCity");
 
 
 
@@ -22,36 +15,44 @@ function handleInputSubmit(event) {
     event.preventDefault();
 
     var cityName = searchInput.value;
-    var cityEl = document.createElement('h2');
-    cityEl.textContent = cityName + now;
-    current.append(cityEl);
-
+    currentCity.textContent = cityName + now;
 
     fetchCoords(cityName);
     searchInput.value = '';
 
+
+    function fetchCoords(city) {
+        // template literal
+        var apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
+
+        fetch(apiURL)
+            .then(function (response) {
+                response.json()
+                    .then(function (data) {
+                        // variables for latitude and longitude coordinates
+                        var lat = data.coord.lat;
+                        var lon = data.coord.lon;
+                        console.log(city);
+                        console.log("Latitude: " + lat);
+                        console.log("Longitude: " + lon);
+                        // use coordinates in api url
+                        var oneCall = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&units=imperial&appid=${apiKey}`;
+
+                        // get UV index
+                        fetch(oneCall)
+                            .then(function (response) {
+                                response.json()
+                                    .then(function (data) {
+                                        var uvi = (data.daily[0].uvi);
+                                        console.log("UVI: " + uvi);
+
+                                    })
+                            });
+                    })
+            })
+    };
 }
 //---------------------------------------End handleInputSubmit Function------------------------------------------
-
-
-
-
-function fetchCoords(city) {
-    
-    var apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
-
-    fetch(apiURL)
-        .then(function (response) {
-            response.json()
-                .then(function (data) {
-                    var lat = data.coord.lat;
-                    var lon = data.coord.lon;
-                    console.log(city,lat,lon);     
-                    console.log(data);                                   
-                })
-        })
-}
-
 
 
 searchForm.addEventListener('submit', handleInputSubmit);
